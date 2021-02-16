4a9823866462fb8705a4bdebe0c5d98ba775bbd3
filipp218 <64597863+filipp218@users.noreply.github.com>
2021-02-16 14:47:31 +0300

    Update yandex.py

--- yandex.py
+++ yandex.py
@@ -1,14 +1,14 @@
 def k_elem(elements):
     """
     >>>k_elem([6, 7, 100])
-    4
-    6
-    1271204700958
+    7
+    10
+    27989015102840550
     """
     al = set(elements)
     max_elem = max(elements)
     start = [0, 1, 2]
     for i in range(3, max_elem+1):
-        start[0],start[1], start[2] = start[1],start[2], start[1]+start[0]
+        start[0],start[1], start[2] = start[1],start[2], start[2]+start[0]
         if i in al:
             print(start[2])
