8e62bc31046bb67c6e2351b43da16be09d098565
filipp218 <64597863+filipp218@users.noreply.github.com>
2021-02-16 22:02:12 +0300

    Update yandex2.py

--- yandex2.py
+++ yandex2.py
@@ -16,9 +16,9 @@
 
 def len_compress(text):
     """
-    >>>len_compress('A4B3')
+    >>> len_compress('A4B3')
     7
-    >>>len_compress('A15BA5')
+    >>> len_compress('A15BA5')
     21
     """
     alphabet = {'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'}
