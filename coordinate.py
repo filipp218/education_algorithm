3d87577790b4532b0d5553abbe79ea85027d7cc0
filipp218 <64597863+filipp218@users.noreply.github.com>
2021-07-19 18:40:40 +0300

    Update coordinate.py

--- coordinate.py
+++ coordinate.py
@@ -1,3 +1,39 @@
+
+import heapq
+
+
+class Solution(object):
+    def way(self, i):
+        a = [0, 0]
+        return -(((i[0] - a[0]) ** 2) + ((i[1] - a[1]) ** 2)) ** 0.5
+
+    def kClosest(self, points, k):
+        """
+        :type points: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        result = []
+        window = [(self.way(i), i) for i in points[:k]]
+        heapq.heapify(window)
+        for i in points[k:]:
+            distance = self.way(i)
+            max_dist, _ = window[0]
+            if distance > max_dist:
+                heapq.heappushpop(window, (distance, i))
+        for dis, point in window:
+            result.append(point)
+        return result
+
+
+
+
+
+
+
+
+
+
 class Solution(object):
     def way(self, i):
         a = [0, 0]
